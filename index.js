3c4d611266a51df065a2de0806d29524c5e7ecb2
Thien-2008 <doanthanhthien23042008@gmail.com>
2026-04-03 22:51:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,18 @@
+
 const TelegramBot = require('node-telegram-bot-api');
 
+// Lấy token từ ENV (Render)
 const token = process.env.TOKEN;
 
-// check token
 if (!token) {
-  console.log("❌ Thiếu TOKEN rồi!");
+  console.error("❌ Thiếu TOKEN!");
   process.exit(1);
 }
 
 const bot = new TelegramBot(token, { polling: true });
 
-console.log("🤖 Bot đang chạy...");
-
-// lệnh start
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "🚀 Bot boost TikTok\nGửi link video để bắt đầu!");
+  bot.sendMessage(msg.chat.id, "OK bot hoạt động 🚀\nBot đã sẵn sàng 😎");
 });
 
-// xử lý tin nhắn
-bot.on('message', (msg) => {
-  const text = msg.text;
-
-  if (!text || text.startsWith("/")) return;
-
-  if (text.includes("tiktok.com")) {
-    bot.sendMessage(msg.chat.id, "⏳ Đang xử lý link...");
-
-    setTimeout(() => {
-      bot.sendMessage(msg.chat.id, "🔥 +1000 views\n❤️ +200 likes\n👀 Done!");
-    }, 2000);
-
-  } else {
-    bot.sendMessage(msg.chat.id, "❌ Gửi link TikTok hợp lệ!");
-  }
-});
+console.log("🤖 Bot đang chạy...");
