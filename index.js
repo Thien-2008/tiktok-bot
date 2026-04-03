9e9ebd69c95b6640f87c90fb3d3cf727eeddb45e
Thien-2008 <doanthanhthien23042008@gmail.com>
2026-04-03 22:31:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,3 +37,14 @@ bot.on("message", (msg) => {
 bot.onText(/\/start/, (msg) => {
   bot.sendMessage(msg.chat.id, "Bot đã sẵn sàng 😎");
 });
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is running");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Web server running on port", PORT);
+});
